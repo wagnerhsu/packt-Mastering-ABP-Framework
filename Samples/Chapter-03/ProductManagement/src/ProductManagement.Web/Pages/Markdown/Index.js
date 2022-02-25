ddccd69570daebb4facdf6c77daba78977ac0bac
Wagner Xu <wagnerhsu@hotmail.com>
2022-02-25 22:43:02 +0800

    Test editor.md

---
+++ Samples/Chapter-03/ProductManagement/src/ProductManagement.Web/Pages/Markdown/Index.js
@@ -0,0 +1,14 @@
+﻿
+$(function () {
+    var editor = editormd("test-editor", {
+        width: "100%",
+        height: "100%",
+        path: "libs/editor.md/lib/",
+        theme: "dark",
+        //previewTheme: "dark",
+        //editorTheme: "pastel-on-dark",
+        //markdown: md,
+        //codeFold: true,
+        //emoji: true,
+    });
+});
--- Samples/Chapter-03/ProductManagement/src/ProductManagement.Web/abp.resourcemapping.js
+++ Samples/Chapter-03/ProductManagement/src/ProductManagement.Web/abp.resourcemapping.js
@@ -1,8 +1,11 @@
 module.exports = {
     aliases: {
-        
+
     },
     mappings: {
-        
+        // Copy all sub-directories
+        "@node_modules/editor.md/css/**": "@libs/editor.md/css/",
+        "@node_modules/editor.md/lib/**": "@libs/editor.md/lib/",
+        "@node_modules/editor.md/editor*.js": "@libs/editor.md/js/"
     }
 };
