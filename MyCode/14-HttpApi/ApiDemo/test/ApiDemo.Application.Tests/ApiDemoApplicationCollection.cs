7651862ac85cc962231dadf84094c9dc411e0146
Wagner Xu <wagnerhsu@hotmail.com>
2024-06-15 10:29:21 +0800

    feat: add 14-HttpApiDemo

---
+++ MyCode/14-HttpApi/ApiDemo/test/ApiDemo.Application.Tests/ApiDemoApplicationCollection.cs
@@ -0,0 +1,10 @@
+﻿using ApiDemo.EntityFrameworkCore;
+using Xunit;
+
+namespace ApiDemo;
+
+[CollectionDefinition(ApiDemoTestConsts.CollectionDefinitionName)]
+public class ApiDemoApplicationCollection : ApiDemoEntityFrameworkCoreCollectionFixtureBase
+{
+
+}
