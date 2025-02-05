48a0dd8ca6a20f66c1ad5d24d4de83652269f8fe
Wagner Xu <wagnerhsu@hotmail.com>
2025-02-05 16:10:40 +0800

    feat: AbpConsole

---
+++ v9/AbpConsole/src/AbpConsole/AbpConsoleHostedService.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Volo.Abp;
+
+namespace AbpConsole;
+
+public class AbpConsoleHostedService : IHostedService
+{
+    private readonly HelloWorldService _helloWorldService;
+
+    public AbpConsoleHostedService(HelloWorldService helloWorldService)
+    {
+        _helloWorldService = helloWorldService;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await _helloWorldService.SayHelloAsync();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
